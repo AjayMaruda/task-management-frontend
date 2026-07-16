e3f88b2c95d150b0c3b8314621f0b2ddddffeb3b
user.name: Ajay Maruda <ajaymarooda@gmail.com>
2026-07-16 18:42:17 +0530

    fix: remove unused PrimeReactProvider to fix license banner and add vercel.json SPA rewrites

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { PrimeReactProvider } from '@primereact/core';
-import Aura from '@primeuix/themes/aura';
 import { store } from './store';
 import router from './routes';
 import { ToastManager } from './components/ToastManager';
@@ -10,10 +8,8 @@ import { ToastManager } from './components/ToastManager';
 const App: React.FC = () => {
   return (
     <Provider store={store}>
-      <PrimeReactProvider theme={{ preset: Aura }}>
-        <ToastManager />
-        <RouterProvider router={router} />
-      </PrimeReactProvider>
+      <ToastManager />
+      <RouterProvider router={router} />
     </Provider>
   );
 };
